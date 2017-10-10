ebc8c6b57b8527b84ba596b359ebd8da0e3763dd
Bojan Rajh <schtr4jh@schtr4jh.net>
2017-10-10 18:42:38 +0200

    Initial commit

---
+++ src/Pckg/Cache/Cache.php
@@ -0,0 +1,66 @@
+<?php namespace Pckg\Cache;
+
+class Cache
+{
+
+    protected $cache = [];
+
+    protected $cachePath;
+
+    protected $built = false;
+
+    public function __construct($cachePath = null)
+    {
+        if ($cachePath) {
+            $this->cachePath = path('cache') . 'framework' . path('ds') . str_replace(['\\', '/', ':'], '_', $cachePath);
+        }
+
+        $this->readFromCache();
+
+        if (!$this->built) {
+            $this->buildCache();
+            $this->writeToCache();
+        }
+    }
+
+    protected function readFromCache()
+    {
+        $file = $this->getCachePath();
+
+        if (file_exists($file)) {
+            $this->cache = json_decode(file_get_contents($file), true);
+            $this->built = true;
+        }
+    }
+
+    protected function getCachePath()
+    {
+        return $this->cachePath;
+    }
+
+    protected function buildCache()
+    {
+    }
+
+    public function writeToCache($cache = null)
+    {
+        if ($cache) {
+            $this->cache = $cache;
+        }
+
+        $cachePath = $this->getCachePath();
+
+        @file_put_contents($cachePath, json_encode($this->cache));
+    }
+
+    public function isBuilt()
+    {
+        return $this->built;
+    }
+
+    public function get()
+    {
+        return $this->cache;
+    }
+
+}
